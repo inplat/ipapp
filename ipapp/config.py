f35797c4827041cc1a31d6b74e73fae83c2ec012
Dmitry Prazdnichnov <dprazdnichnov@inplat.ru>
2019-11-29 14:49:16 +0300

    Fix: yaml loader и dumper в конфигурации заменены на безопасные версии

--- ipapp/config.py
+++ ipapp/config.py
@@ -1,63 +1,166 @@
-import os
-from typing import Any, Dict, Mapping, Optional, Union
+from __future__ import annotations
 
-from pydantic.env_settings import BaseSettings, SettingsError
-from pydantic.fields import SHAPE_LIST
+import json
+import os
+import sys
+from io import BufferedIOBase, RawIOBase, TextIOBase
+from typing import (
+    IO,
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Mapping,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+)
+
+import yaml
+from pydantic.fields import SHAPE_SINGLETON
+from pydantic.main import BaseModel, Extra
+from yaml import SafeDumper, SafeLoader
 
 __all__ = ("BaseConfig",)
 
+T = TypeVar("T", bound="BaseConfig")
+IO_TYPES = (RawIOBase, TextIOBase, BufferedIOBase)
 
-class BaseConfig(BaseSettings):
-    def get_environ(self) -> Mapping[str, str]:
-        return os.environ
 
-    def _build_environ(self) -> Dict[str, Optional[str]]:
-        """
-        Build environment variables suitable for passing to the Model.
-        """
+class BaseConfig(BaseModel, Generic[T]):
+    @classmethod
+    def _filter_dict(
+        cls: Type[T],
+        input_dict: Mapping[str, Any],
+        prefix: str,
+        trim_prefix: bool = True,
+    ) -> Mapping[str, Any]:
+        output_dict: Dict[str, Any] = {}
+        for key, value in input_dict.items():
+            if not cls.__config__.case_sensitive:
+                key = key.lower()
+                prefix = prefix.lower()
+
+            if key.startswith(prefix):
+                output_dict[key[len(prefix) :] if trim_prefix else key] = value
+
+        return output_dict
+
+    @classmethod
+    def from_env(cls: Type[T], prefix: str = "") -> T:
         d: Dict[str, Optional[Any]] = {}
+        env_vars = cls._filter_dict(os.environ, prefix)
 
-        if self.__config__.case_sensitive:
-            env_vars: Mapping[str, str] = self.get_environ()
+        for field in cls.__fields__.values():
+            deprecated = field.field_info.extra.get("deprecated", False)
+            if deprecated:
+                print(
+                    f"WARNING: {field.name} field is deprecated",
+                    file=sys.stderr,
+                )
+
+            field_prefix = field.field_info.extra.get(
+                "env_prefix", f"{field.name}_",
+            )
+
+            if field.shape == SHAPE_SINGLETON and issubclass(
+                field.type_, BaseModel
+            ):
+                field_values = cls._filter_dict(env_vars, field_prefix)
+                d[field.alias] = field.type_(**field_values)
+
+        return cls(**d)
+
+    def to_env(self) -> Dict[str, str]:
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(cls: Type[T], input_dict: Dict[str, Any]) -> T:
+        return cls(**input_dict)
+
+    def to_dict(self, **kwargs: Any) -> Dict[str, Any]:
+        return self.dict(**kwargs)
+
+    @classmethod
+    def from_json(
+        cls: Type[T],
+        stream: Union[str, IO],
+        loads: Optional[Callable] = None,
+        **kwargs: Any,
+    ) -> T:
+        loads = loads or cls.__config__.json_loads
+        string: Optional[str] = None
+
+        if isinstance(stream, str):
+            with open(stream) as f:
+                string = f.read()
+        elif isinstance(stream, IO_TYPES):
+            string = stream.read()
         else:
-            env_vars = {k.lower(): v for k, v in self.get_environ().items()}
+            raise ValueError
 
-        for field in self.__fields__.values():
+        return cls(**loads(string, **kwargs))
 
-            val: Optional[Any] = None
+    def to_json(self, stream: Union[str, IO], **kwargs: Any) -> None:
+        data = self.json(**{"indent": 4, **kwargs})  # type: ignore
 
-            if field.shape == SHAPE_LIST:
-                val = []
-                if field.type_.__origin__ == Union:
-                    for arg in field.type_.__args__:
-                        prefix = self.Config.env_prefix + arg.Config.env_prefix
-                        kwargs = {}
-                        for key, value in env_vars.items():
-                            if key.startswith(prefix):
-                                pl = len(prefix)
-                                kwargs[key[pl:]] = value
-                        if kwargs:
-                            val.append(arg(**kwargs))
-                d[field.alias] = val
-                continue
+        if isinstance(stream, str):
+            with open(stream, "w") as f:
+                f.write(data)
+        elif isinstance(stream, IO_TYPES):
+            stream.write(data)
+        else:
+            raise ValueError
 
-            name: Optional[str] = None
-            for env_name in field.field_info.extra['env_names']:
-                val = env_vars.get(env_name)
-                if val is not None:
-                    break
-                name = env_name
+    @classmethod
+    def from_yaml(
+        cls: Type[T],
+        stream: Union[str, IO],
+        load: Optional[Callable] = None,
+        **kwargs: Any,
+    ) -> T:
+        load = load or cls.__config__.yaml_load
+        string: Optional[str] = None
 
-            if val is None or name is None:
-                continue
+        if isinstance(stream, str):
+            with open(stream) as f:
+                string = f.read()
+        elif isinstance(stream, IO_TYPES):
+            string = stream.read()
+        else:
+            raise ValueError
 
-            if field.is_complex():
-                try:
-                    val = self.__config__.json_loads(val)  # type: ignore
-                except ValueError as e:
-                    raise SettingsError(
-                        f'error parsing JSON for "{name}"'
-                    ) from e
-            d[field.alias] = val
+        return cls(**load(string, **{"Loader": SafeLoader, **kwargs}))
 
-        return d
+    def to_yaml(
+        self,
+        stream: Union[str, IO],
+        dump: Optional[Callable] = None,
+        **kwargs: Any,
+    ) -> None:
+        dump = dump or self.__config__.yaml_dump
+
+        json_str = self.json(**{"indent": 4, **kwargs})  # type: ignore
+        json_obj = self.__config__.json_loads(json_str)
+        yaml_str = dump(json_obj, **{"Dumper": SafeDumper, **kwargs})
+
+        if isinstance(stream, str):
+            with open(stream, "w") as f:
+                f.write(yaml_str)
+        elif isinstance(stream, IO_TYPES):
+            stream.write(yaml_str)
+        else:
+            raise ValueError
+
+    class Config:
+        validate_all = True
+        extra = Extra.forbid
+        arbitrary_types_allowed = True
+        case_sensitive = False
+        json_loads: Callable = json.loads
+        json_dumps: Callable = json.dumps
+        yaml_load: Callable = yaml.load
+        yaml_dump: Callable = yaml.dump
+
+    __config__: Config  # type: ignore
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,51 +1,194 @@
-from typing import List, Mapping, Optional, Union
+import io
+import os
+from tempfile import NamedTemporaryFile
+from typing import Any
 
-from pydantic import BaseModel, BaseSettings
+from pytest import raises
+from pydantic import BaseModel, Field
 
 from ipapp.config import BaseConfig
 
 
-def test_cfg():
-    env = {
-        'APP_ZIPKIN_LEVEL': 'INFO',
-        'APP_ZIPKIN_URL': 'http://zipkin',
-        'APP_PROMETHEUS_URL': 'http://prometheus',
-        'APP_DB_URL': 'postgres',
+class Logger(BaseModel):
+    level: str = "DEBUG"
+
+
+class ZipkinLogger(Logger):
+    url: str = "http://localhost:9411"
+
+
+class PrometheusLogger(Logger):
+    url: str = "http://localhost:9213"
+
+
+class Postgres(BaseModel):
+    url: str = "postgres://user:pass@localhost:5432/db"
+
+
+class Config(BaseConfig):
+    db: Postgres = Postgres()
+    db1: Postgres = Field(..., deprecated=True)
+    db2: Postgres = Field(..., env_prefix="database2_")
+    zipkin: ZipkinLogger
+    prometheus: PrometheusLogger
+
+
+def validate(config: Any) -> None:
+    assert config.zipkin.level == "INFO"
+    assert config.zipkin.url == "http://jaeger:9411"
+    assert config.prometheus.level == "DEBUG"
+    assert config.prometheus.url == "http://prometheus:9213"
+    assert config.db.url == "postgres://user:pass@localhost:5432/db"
+    assert config.db1.url == "postgres://user:pass@localhost:8001/db"
+    assert config.db2.url == "postgres://user:pass@localhost:9002/db"
+
+
+def test_from_env() -> None:
+    os.environ["APP_ZIPKIN_LEVEL"] = "INFO"
+    os.environ["APP_ZIPKIN_URL"] = "http://jaeger:9411"
+    os.environ["APP_PROMETHEUS_URL"] = "http://prometheus:9213"
+    os.environ["APP_DB1_URL"] = "postgres://user:pass@localhost:8001/db"
+    os.environ["APP_DATABASE2_URL"] = "postgres://user:pass@localhost:9002/db"
+
+    config: Config = Config.from_env(prefix="app_")
+    validate(config)
+
+
+def test_from_dict() -> None:
+    dictionary = {
+        "zipkin": {"level": "INFO", "url": "http://jaeger:9411"},
+        "prometheus": {"url": "http://prometheus:9213"},
+        "db1": {"url": "postgres://user:pass@localhost:8001/db"},
+        "db2": {"url": "postgres://user:pass@localhost:9002/db"},
     }
 
-    class Logger(BaseModel):
-        level: str = 'DEBUG'
+    config: Config = Config.from_dict(dictionary)
+    validate(config)
 
-    class ZipkinLogger(Logger):
-        url: str = 'http://localhost'
 
-        class Config:
-            env_prefix = 'zipkin_'
+def test_to_dict() -> None:
+    config: Config = Config.from_env(prefix="app_")
+    dictionary = config.to_dict()
+    assert dictionary == {
+        "db": {"url": "postgres://user:pass@localhost:5432/db"},
+        "db1": {"url": "postgres://user:pass@localhost:8001/db"},
+        "db2": {"url": "postgres://user:pass@localhost:9002/db"},
+        "prometheus": {"level": "DEBUG", "url": "http://prometheus:9213"},
+        "zipkin": {"level": "INFO", "url": "http://jaeger:9411"},
+    }
 
-    class PrometheusLogger(Logger):
-        url: str = 'http://localhost'
 
-        class Config:
-            env_prefix = 'prometheus_'
+def test_from_json_stream() -> None:
+    stream = io.StringIO(
+        """
+    {
+        "zipkin": {
+            "level": "INFO",
+            "url": "http://jaeger:9411"
+        },
+        "prometheus": {
+            "url": "http://prometheus:9213"
+        },
+        "db1": {
+            "url": "postgres://user:pass@localhost:8001/db"
+        },
+        "db2": {
+            "url": "postgres://user:pass@localhost:9002/db"
+        }
+    }
+    """
+    )
 
-    class DbCfg(BaseSettings):
-        url: Optional[str] = None
+    config: Config = Config.from_json(stream)
+    validate(config)
 
-    class Config(BaseConfig):
-        db: DbCfg = DbCfg()
-        loggers: List[Union[ZipkinLogger, PrometheusLogger]]
 
-        class Config:
-            env_prefix = 'app_'
+def test_from_json_file() -> None:
+    config: Config = Config.from_json("tests/config.json")
+    validate(config)
 
-        def get_environ(self) -> Mapping[str, str]:
-            return env
+    with raises(ValueError):
+        Config.from_json(b"")  # type: ignore
 
-    cfg = Config()
-    print(cfg)
-    # assert cfg.db.url is None
-    assert len(cfg.loggers) == 2
-    assert cfg.loggers[0].url == 'http://zipkin'
-    assert cfg.loggers[0].level == 'INFO'
-    assert cfg.loggers[1].url == 'http://prometheus'
-    assert cfg.loggers[1].level == 'DEBUG'
+
+def test_to_json_stream() -> None:
+    config: Config = Config.from_env(prefix="app_")
+    stream = io.StringIO()
+    config.to_json(stream)
+
+    with raises(ValueError):
+        config.to_json(b"")  # type: ignore
+
+    stream.seek(0)
+    temp_conf = Config.from_json(stream)
+    validate(temp_conf)
+
+
+def test_to_json_file() -> None:
+    config: Config = Config.from_env(prefix="app_")
+    try:
+        temp = NamedTemporaryFile()
+        config.to_json(temp.name)
+        temp_conf = Config.from_json(temp.name)
+        validate(temp_conf)
+    finally:
+        temp.close()
+
+
+def test_from_yaml_stream() -> None:
+    stream = io.StringIO(
+        """
+    zipkin:
+        level: INFO
+        url: http://jaeger:9411
+    prometheus:
+        url: http://prometheus:9213
+    db1:
+        url: postgres://user:pass@localhost:8001/db
+    db2:
+        url: postgres://user:pass@localhost:9002/db
+    """
+    )
+
+    config: Config = Config.from_yaml(stream)
+    validate(config)
+
+
+def test_from_yaml_file() -> None:
+    config: Config = Config.from_yaml("tests/config.yaml")
+    validate(config)
+
+    with raises(ValueError):
+        Config.from_yaml(b"")  # type: ignore
+
+
+def test_to_yaml_stream() -> None:
+    config: Config = Config.from_env(prefix="app_")
+    stream = io.StringIO()
+    config.to_yaml(stream)
+
+    with raises(ValueError):
+        config.to_yaml(b"")  # type: ignore
+
+    stream.seek(0)
+    temp_conf = Config.from_yaml(stream)
+    validate(temp_conf)
+
+
+def test_to_yaml_file() -> None:
+    config: Config = Config.from_env(prefix="app_")
+    try:
+        temp = NamedTemporaryFile()
+        config.to_yaml(temp.name)
+        temp_conf = Config.from_yaml(temp.name)
+        validate(temp_conf)
+    finally:
+        temp.close()
+
+
+def test_deprecated_field(capsys: Any) -> None:
+    os.environ["APP_DB1_URL"] = "postgres://user:pass@localhost:8001/db"
+    Config.from_env(prefix="app_")
+
+    captured = capsys.readouterr()
+    assert captured.err == "WARNING: db1 field is deprecated\n"
